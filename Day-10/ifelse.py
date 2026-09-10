dc5b103f40d965cc2b16f862961be873927a8619
Kotla Shashivardhan Reddy <shashivardhankotla23@gmail.com>
2026-09-10 18:53:52 +0530

    Conditional Statements

---
+++ Day-10/ifelse.py
@@ -0,0 +1,6 @@
+username = input("Username: ")
+password = input("Password: ")
+if username == "vardhan" and password == "vardhan@23":
+    print("Login Successful")
+else:
+    print("Invalid Credentials")    
