7bbcc5ff20b3a4904731630cc5bb3d0cee4df770
Kotla Shashivardhan Reddy <shashivardhankotla23@gmail.com>
2026-09-11 11:57:54 +0530

    Data & Time Module

---
+++ Day-24/builtin.py
@@ -0,0 +1,10 @@
+import random
+
+name = input("Enter the name: ").title()
+dob = input("Enter the DOB[DD-MM-YYYY]:")
+spc = ['@','!','#','$','%','&','*','+','.',',']
+
+password = name+random.choice(spc)+dob[-4:]
+
+print("Generated Password:",password)
+                                    
